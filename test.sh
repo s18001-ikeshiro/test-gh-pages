11b0779d8db3ec19d8bdcf3acf95401fbdc99b8e
s18001 <s18001@std.it-college.ac.jp>
2018-05-11 07:11:31 +0000

    test.shを作成

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+today=$(date '+%d')
+
+#echo $today
+ 
+if [ $today -eq 20 ]; then
+  echo "今日は２０日です。月次〆処置を開始してください"
+elif [ $today -lt 20 ]; then
+  echo "今日は２０日より前の日にちです"
+else
+  echo "今日は２０日ではありません。"
+fi
